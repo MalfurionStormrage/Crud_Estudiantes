f6eb23a7ba90d11ae47f5990178a512dbd0731f5
Abraham David Lara Rodriguez <abrahamopelara@hotmail.com>
2021-09-18 12:16:49 -0500

    end of 18-09-2021

--- Capa_Presentacion/My Project/Application.Designer.vb
+++ Capa_Presentacion/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Capa_Presentacion.Form_Agregar_Notas
+            Me.MainForm = Global.Capa_Presentacion.Aplicacion
         End Sub
     End Class
 End Namespace
